0abd36c80fd2c2aaa37d92af109068fb1563df57
mananas <tecnoedix@icloud.com>
2025-05-08 12:32:48 +0200

    prueba

--- IMC-Calculator-IOS/ViewController.swift
+++ IMC-Calculator-IOS/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var PesoLabel: UILabel!
+    @IBOutlet weak var AlturaLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var weight = 60
+    var height = 200
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func weightChangeButton(_ sender: UIStepper) {
+        weight = Int(sender.value)
+        PesoLabel.text = "\(weight) KG"
+    }
+    
+    @IBAction func heightChangeSlider(_ sender: UISlider) {
+        height = Int(sender.value)
+        AlturaLabel.text = "\(height) CM"
+    }
+    @IBAction func calcButton(_ sender: Any) {
+        
+        let result = Float(weight) / pow
+        
+
+     }
 
 }
 
